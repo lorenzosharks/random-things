023ff1682934b50896c7711ebdd05a97b4e6ee02
lorenzosharks <karsonx27@lakesideschool.org>
2025-09-16 23:01:26 -0700

    space

--- latin.py
+++ latin.py
@@ -6,4 +6,4 @@ number = ["singular", "plural"]
 
 verb = ["2", "3", "3i", "4"]
 
-print(random.choice(person) + " person, " + random.choice(number) + ", " + random.choice(verb) + "declension")
+print(random.choice(person) + " person, " + random.choice(number) + ", " + random.choice(verb) + " declension")
